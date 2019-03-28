cbda67f5caae66fa4c15b1808ed71509e43dc943
denghe <denghe@hotmail.com>
2019-03-29 01:01:45 +0800

    compile bug fix

--- gens/pkggen_template_PKG/Global.cs
+++ gens/pkggen_template_PKG/Global.cs
@@ -13,27 +13,27 @@ class Error
     string txt;
 }
 
-[Desc("服务连接信息")]
-class ConnInfo
-{
-    [Desc("服务类型")]
-    Server.Types type;
+//[Desc("服务连接信息")]
+//class ConnInfo
+//{
+//    [Desc("服务类型")]
+//    Server.Types type;
 
-    [Desc("ipv4/6 地址")]
-    string ip;
+//    [Desc("ipv4/6 地址")]
+//    string ip;
 
-    [Desc("端口")]
-    int port;
+//    [Desc("端口")]
+//    int port;
 
-    [Desc("令牌")]
-    string token;
-}
+//    [Desc("令牌")]
+//    string token;
+//}
 
-[Desc("并非一般的数据包. 仅用于声明各式 List<T>")]
-class Collections
-{
-    List<int> ints;
-    List<long> longs;
-    List<string> strings;
-    List<object> objects;
-}
+//[Desc("并非一般的数据包. 仅用于声明各式 List<T>")]
+//class Collections
+//{
+//    List<int> ints;
+//    List<long> longs;
+//    List<string> strings;
+//    List<object> objects;
+//}
--- test_uv_a/main.cpp
+++ test_uv_a/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "xx_uv.h"
+#include "PKG_class.h"
 
 struct EchoPeer : xx::UvKcpPeer {
 	using BaseType = xx::UvKcpPeer;
@@ -6,18 +7,18 @@ struct EchoPeer : xx::UvKcpPeer {
 
 	std::shared_ptr<EchoPeer> holder;
 	virtual void Disconnect() noexcept override {
-		xx::Cout(guid, " disconnected.\n");
+		xx::CoutN(GetIP(), " ", guid, " disconnected.");
 		holder = nullptr;					// release peer
 	}
 	virtual int ReceivePush(xx::Object_s&& msg) noexcept override {
-		//xx::CoutN("recv push ", msg);
+		xx::CoutN("recv push ", msg);
 		if (int r = SendPush(msg)) return r;
 		Flush();
 		ResetTimeoutMS(3000);
 		return 0;
 	}
 	virtual int ReceiveRequest(int const& serial, xx::Object_s&& msg) noexcept override {
-		//xx::CoutN("recv request serial = ", serial, ", msg = ", msg);
+		xx::CoutN("recv request serial = ", serial, ", msg = ", msg);
 		if (int r = SendResponse(serial, msg)) return r;
 		Flush();
 		ResetTimeoutMS(3000);
@@ -33,13 +34,13 @@ struct EchoPeerListener : xx::UvKcpListener<EchoPeer> {
 		auto&& peer = xx::As<EchoPeer>(peer_);
 		peer->holder = peer;				// hold memory
 		peer->ResetTimeoutMS(3000);			// 设置 3 秒内没收到能解析出来的合法包就 Dispose
-		xx::Cout(peer->guid, " accepted.\n");
+		xx::CoutN(peer->GetIP(), " ", peer->guid, " accepted.");
 	}
 };
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		std::cout << "need args: port\n";
+		xx::CoutN("need args: port");
 		return -1;
 	}
 	xx::Uv uv;
