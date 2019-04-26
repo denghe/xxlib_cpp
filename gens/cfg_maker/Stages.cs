a0ffff3645d13f91909774b3f86408e3925e139b
denghe <denghe@hotmail.com>
2019-04-26 18:32:58 +0800

    minor

--- gens/cfg_maker/Stages.cs
+++ gens/cfg_maker/Stages.cs
@@ -22,31 +22,33 @@
                 e1.cfg_endTicks /= 2;                                   // 停止运作时间为关卡时长的一半
                 e1.cfg_angleBegin = 0;                                  // 起始角度
                 e1.cfg_angleIncrease = (float)(System.Math.PI / 30);    // 每生成一只鱼的累加角度
-                e1.cfg_bornTicksInterval = 2;                           // 两只鱼生成帧间隔
+                e1.cfg_bornTicksInterval = 3;                           // 两只鱼生成帧间隔
                 e1.cfg_speed = 3;                                       // 每帧移动像素跨度
                 e1.cfg_scale = 1;                                       // 体积
                 e1.cfg_coin = 1;
                 e1.angle = e1.cfg_angleBegin;
-
-                // 旋转出鱼--180
-                var e2 = CloneElement(e1);
-                e2.angle = e2.cfg_angleBegin = (float)System.Math.PI;
+                {
+                    // 旋转出鱼--180
+                    var e1a = CloneElement(e1);
+                    e1a.angle = e1a.cfg_angleBegin = (float)System.Math.PI;
+                }
 
                 // 圆环鱼阵--小鱼
-                var e3 = NewElement<PKG.CatchFish.Stages.Emitter_RingFishs>(s);
-                e3.cfg_beginTicks = e1.cfg_endTicks;                    // 生效时间：位于 旋转出鱼 结束之后
-                e3.cfg_numFishsPerBatch = 36;                           // 每波产生多少只
-                e3.cfg_bornTicksInterval = 60;                          // 波与波之间生成帧间隔
-                e3.cfg_speed = 3;
-                e3.cfg_scale = 1;
-                e3.cfg_coin = 1;
-
-                // 圆环鱼阵--中鱼( 与 圆环鱼阵--小鱼 交叠起作用 )
-                var e4 = CloneElement(e3);
-                e4.cfg_beginTicks += e3.cfg_bornTicksInterval / 2;      // 生效时间：比 圆环鱼阵--小鱼 晚 波与波之间生成帧间隔 / 2
-                e4.cfg_numFishsPerBatch = 18;
-                e4.cfg_scale = 2;
-                e4.cfg_coin = 2;
+                var e2 = NewElement<PKG.CatchFish.Stages.Emitter_RingFishs>(s);
+                e2.cfg_beginTicks = e1.cfg_endTicks;                    // 生效时间：位于 旋转出鱼 结束之后
+                e2.cfg_numFishsPerBatch = 36;                           // 每波产生多少只
+                e2.cfg_bornTicksInterval = 60;                          // 波与波之间生成帧间隔
+                e2.cfg_speed = 3;
+                e2.cfg_scale = 1;
+                e2.cfg_coin = 1;
+                {
+                    // 圆环鱼阵--中鱼( 与 圆环鱼阵--小鱼 交叠起作用 )
+                    var e2a = CloneElement(e2);
+                    e2a.cfg_beginTicks += e2.cfg_bornTicksInterval / 2;      // 生效时间：比 圆环鱼阵--小鱼 晚 波与波之间生成帧间隔 / 2
+                    e2a.cfg_numFishsPerBatch = 18;
+                    e2a.cfg_scale = 2;
+                    e2a.cfg_coin = 2;
+                }
             }
             {
                 // 自动补大鱼
