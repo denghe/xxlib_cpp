b33e24b7137e749592cd66d20051cd9155ae32b8
denghe <denghe@hotmail.com>
2019-06-17 21:17:55 +0800

    writing

--- client_test1/main.cpp
+++ client_test1/main.cpp
@@ -92,7 +92,16 @@ using Results = std::array<Result, linesLen * 2>;
 inline void Dump(Grid const& grid) {
 	for (int j = 0; j < 3; ++j) {
 		for (int i = 0; i < 5; ++i) {
-			std::cout << grid[j * 5 + i] << " ";
+			auto&& v = grid[j * 5 + i];
+			if (i) {
+				std::cout << " ";
+			}
+			if (v == -1) {
+				std::cout << "_";
+			}
+			else {
+				std::cout << v;
+			}
 		}
 		std::cout << std::endl;
 	}
@@ -210,7 +219,7 @@ union ShapeNums {
 	uint32_t value;
 };
 
-union MaskKey {
+union LineMask {
 	struct {
 		uint8_t index : 4;
 		uint8_t direction : 1;
@@ -219,64 +228,212 @@ union MaskKey {
 	char value;
 };
 
-
-int main() {
-	Lines lines = {
-		5, 6, 7, 8, 9,
-		0, 1, 2, 3, 4,
-		10, 11, 12, 13, 14,
-		0, 6, 12, 8, 4,
-		10, 6, 2, 8, 14,
-		0, 1, 7, 3, 4,
-		10, 11, 7, 13, 14,
-		5, 11, 12, 13, 9,
-		5, 1, 2, 3, 9,
-	};
-	Grid grid;
-	std::array<int, 4> counts;
-	std::unordered_map<uint32_t, int> shapes;
-	std::unordered_map<uint32_t, std::unordered_set<std::string>> shapesResults;
-	Results results;
-	int resultsLen = 0;
-	GridValues g;
-	for (g.value = 0; g.value <= 0b111111111111111111111111111111; ++g.value) {
-		if ((g.value & 0b11111111111111111111111) == 0) {
-			std::cout << ".";
-			std::cout.flush();
+// 返回二进制的指定线上的 0 掩码
+template<int offset, int step = (offset > 0 ? 1 : -1)>
+inline int CalcLineZeroMask(int const& count, int const* const& line, Grid const& grid) noexcept {
+	int rtv = 0;
+	for (int i = 0; i < count; ++i) {
+		if (!grid[*(line + i * step)]) {
+			rtv |= (1 << i);
 		}
+	}
+	return rtv;
+}
+
+// 返回二进制的 0 分布图
+inline int CalcGridZeroMask(Grid const& grid) noexcept {
+	int rtv = 0;
+	for (int i = 0; i < grid.size(); ++i) {
+		if (!grid[i]) {
+			rtv |= (1 << i);
+		}
+	}
+	return rtv;
+}
+
+
+// 返回 0 的个数
+inline size_t CalcGridZeroCount(Grid const& grid) noexcept {
+	size_t rtv = 0;
+	for (int i = 0; i < grid.size(); ++i) {
+		if (!grid[i]) {
+			++rtv;
+		}
+	}
+	return rtv;
+}
+
+// 返回 0 的平均出现个数( 结果为 3.75 )
+inline void CalcAvgZeroCount() noexcept {
+	Grid grid;
+	GridValues g;
+	size_t count = 0;
+	size_t count2 = 0;
+	for (g.value = 0; g.value <= 0b111111111111111111111111111111; ++g.value) {
 		grid = {
 			g.c0, g.c1, g.c2, g.c3, g.c4,
 			g.c5, g.c6, g.c7, g.c8, g.c9,
 			g.c10, g.c11, g.c12, g.c13, g.c14,
 		};
-		resultsLen = 0;
-		Calc((Result*)& results, resultsLen, grid, lines);
-		if (!resultsLen) continue;
-		if (results[0].index == -1) continue;		// ignore full screen
-		
-		counts.fill(0);
-		std::string maskKeys;
-		for (int i = 0; i < resultsLen; ++i) {
-			++counts[results[i].count - 3];
-			MaskKey k;
-			k.index = results[i].index;
-			k.direction = results[i].direction;
-			k.count = results[i].count;
-			maskKeys.append(&k.value, 1);
+		auto&& n = CalcGridZeroCount(grid);
+		if (n) {
+			count += n;
+			++count2;
 		}
-		auto key = counts[0] | counts[1] << 8 | counts[2] << 16;
-		++shapes[key];
-
-		shapesResults[key].insert(std::move(maskKeys));
 	}
+	std::cout << (double)count / 0b111111111111111111111111111111 << ", " << count2;
+}
 
-	std::cout << shapes.size() << std::endl;
+//int main() {
+//	CalcAvgZeroCount();
+//	return 0;
+//}
 
-	size_t siz = 0;
+
+
+
+//int main() {
+//	Lines lines = {
+//		5, 6, 7, 8, 9,
+//		0, 1, 2, 3, 4,
+//		10, 11, 12, 13, 14,
+//		0, 6, 12, 8, 4,
+//		10, 6, 2, 8, 14,
+//		0, 1, 7, 3, 4,
+//		10, 11, 7, 13, 14,
+//		5, 11, 12, 13, 9,
+//		5, 1, 2, 3, 9,
+//	};
+//	Grid grid;
+//	std::array<int, 4> counts;
+//	std::unordered_map<uint32_t, int> shapes;
+//	std::unordered_map<uint32_t, std::unordered_set<std::string>> shapesResults;
+//	Results results;
+//	int resultsLen = 0;
+//	GridValues g;
+//	for (g.value = 0; g.value <= 0b111111111111111111111111111111; ++g.value) {
+//		if ((g.value & 0b11111111111111111111111) == 0) {
+//			std::cout << ".";
+//			std::cout.flush();
+//		}
+//		grid = {
+//			g.c0, g.c1, g.c2, g.c3, g.c4,
+//			g.c5, g.c6, g.c7, g.c8, g.c9,
+//			g.c10, g.c11, g.c12, g.c13, g.c14,
+//		};
+//		resultsLen = 0;
+//		Calc((Result*)& results, resultsLen, grid, lines);
+//		if (!resultsLen) continue;
+//		if (results[0].index == -1) continue;		// ignore full screen
+//		
+//		counts.fill(0);
+//		std::string masks;
+//		auto zeroMask = (uint16_t)CalcGridZeroMask(grid);
+//		masks.append((char*)& zeroMask, sizeof(zeroMask));
+//		for (int i = 0; i < resultsLen; ++i) {
+//			++counts[results[i].count - 3];
+//			LineMask m;
+//			m.index = results[i].index;
+//			m.direction = results[i].direction;
+//			m.count = results[i].count;
+//			masks.append((char*)&m.value, sizeof(m.value));
+//		}
+//
+//		auto key = counts[0] | counts[1] << 8 | counts[2] << 16;
+//		++shapes[key];
+//
+//		shapesResults[key].insert(std::move(masks));
+//	}
+//
+//	std::cout << shapes.size() << std::endl;
+//
+//	size_t siz = 0;
+//	ShapeNums n;
+//	std::string s;
+//	for (auto&& iter : shapes) {
+//		n.value = iter.first;
+//		s.clear();
+//		if (n.n3) {
+//			s.append("3*");
+//			s.append(std::to_string(n.n3));
+//		}
+//		if (n.n4) {
+//			if (s.size()) {
+//				s.append(", ");
+//			}
+//			s.append("4*");
+//			s.append(std::to_string(n.n4));
+//		}
+//		if (n.n5) {
+//			if (s.size()) {
+//				s.append(", ");
+//			}
+//			s.append("5*");
+//			s.append(std::to_string(n.n5));
+//		}
+//		if (n.n15) {
+//			if (s.size()) {
+//				s.append(", ");
+//			}
+//			s.append("15*");
+//			s.append(std::to_string(n.n5));
+//		}
+//		auto&& numResults = shapesResults[iter.first].size();
+//		siz += numResults;
+//		s = s + " : " + std::to_string(iter.second) + ", shapesResults.size() = " + std::to_string(numResults);
+//		std::cout << s << std::endl;
+//
+//
+//	}
+//	std::cout << "total shapesResults 's size = " << siz << std::endl;
+//	
+//	xx::BBuffer bb;
+//	for (auto&& iter : shapesResults) {
+//		bb.Write(iter.first);				// 3x4y5z
+//		bb.Write(iter.second.size());		// len
+//		for (auto&& str : iter.second) {	// masks( grid zero mask 2 bytes + LineMask 1 byte * n
+//			bb.Write(str);
+//		}
+//	}
+//	xx::WriteAllBytes("huga.bin", bb);
+//	std::cout << "bb.len = " << bb.len << std::endl;		// 最终输出 482131 KB
+//}
+
+inline void FillGridByZeroMask(Grid& grid, std::string const& s) {
+	assert(s.size() > 2);
+	auto mask = *(uint16_t*)s.data();
+	xx::CoutN(mask);
+	for (int i = 0; i < 15; ++i) {
+		if (mask & (1 << i)) {
+			grid[i] = 0;
+		}
+	}
+}
+
+int main() {
+	xx::BBuffer bb;
+	xx::ReadAllBytes("huga.bin", bb);
+	xx::CoutN(bb.len);
+
+	// todo: 还原成字典 并填充权值啥的 准备随机函数 试着随机并填充牌型. 0 直接按照 mask 来填充
+	std::vector<std::pair<uint32_t, std::vector<std::string>>> shapes;
+	size_t len;
+	while (bb.offset < bb.len) {
+		auto&& ss = shapes.emplace_back();
+		if (int r = bb.Read(ss.first)) return r;
+		if (int r = bb.Read(len)) return r;
+		ss.second.reserve(len);
+		for (size_t i = 0; i < len; ++i) {
+			auto&& s = ss.second.emplace_back();
+			if (int r = bb.Read(s)) return r;
+		}
+	}
+	xx::CoutN("shapes.len = ", shapes.size());
 	ShapeNums n;
 	std::string s;
-	for (auto&& iter : shapes) {
-		n.value = iter.first;
+	for (int i = 0; i < shapes.size(); ++i) {
+		auto&& pair = shapes[i];
+		n.value = pair.first;
 		s.clear();
 		if (n.n3) {
 			s.append("3*");
@@ -303,41 +460,35 @@ int main() {
 			s.append("15*");
 			s.append(std::to_string(n.n5));
 		}
-		auto&& numResults = shapesResults[iter.first].size();
-		siz += numResults;
-		s = s + " : " + std::to_string(iter.second) + ", shapesResults.size() = " + std::to_string(numResults);
-		std::cout << s << std::endl;
+		s = s + " : " + std::to_string(pair.second.size());
+		std::cout << "shapes[" << i << "] = " << s << std::endl;
 	}
-	std::cout << "total shapesResults 's size = " << siz << std::endl;
-	
 
-	auto&& path = xx::GetCurrentPath() / L"huga.bin";
-	xx::BBuffer bb;
-	// todo
-	xx::WriteAllBytes(path, bb);
+	// 试定位到某 shape 某 grid zero mask
+	auto&& ss = shapes[25].second;
+	Grid g;
+	for (auto&& s : ss) {
+		g.fill(-1);
+		FillGridByZeroMask(g, s);
+		Dump(g);
+		xx::CoutN();
+	}
+
+	// 填充思路：先填所有 0. 然后 foreach 每线。 每条线的中奖位里面，如果有空位置，就随机一个非 0 填上. 
+	// 如果 有空位但是存在非0， 则用非0 继续填充 ( 避免交叉格子冲突 )
+	// 最后填充杂物. 
+	// 杂物填充大体思路：排除掉 0 和所有线用掉的符号，剩下的交错出现以避免形成新的中奖线条
+	// todo: 杂物填充细节. 总的来说用排除法. 避免和邻居相同
+
+	return 0;
 }
-
 
 
 
 
 /*
 
-线型组合分析( 不考虑全屏 )
-3*18
-11022
-11022
-11022
-
-3*17
-2 2 0 1 0
-0 3 0 1 0
-0 3 0 1 0
-
-..... 约 997 种 shape
-
-
-
+..... 约 993 种 shape + 全屏
 
 */
 
