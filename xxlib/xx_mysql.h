77c3fd785ccd0c35a75dd8243b91363ab2dc848b
denghe <denghe@hotmail.com>
2019-08-19 10:23:28 +0800

    minor fix & improve

--- xxlib/xx_mysql.h
+++ xxlib/xx_mysql.h
@@ -10,30 +10,15 @@ namespace xx
 {
 	namespace MySql {
 
-		// 引用到已有字串 方便 Append 时自动转义. 
+		// 方便 Append 时 使用 mysql_escape_string 转义. xx::Append( sql, "....", xx::MySql::Escaped( "..\\''.." ), "...." )
 		struct Escaped {
-			MYSQL* ctx;
-			char const* buf = nullptr;
-			size_t len = 0;
-
-			Escaped(MYSQL* ctx) : ctx(ctx) {}
-			Escaped() = delete;
+			std::string s;
+			template<typename T>
+			Escaped(T&& s) {
+				this->s = std::forward<T>(s);
+			}
 			Escaped(Escaped const&) = delete;
 			Escaped& operator=(Escaped const&) = delete;
-
-			Escaped& operator=(std::string const& s) {
-				buf = (char*)s.data();
-				len = s.size();
-				return *this;
-			}
-
-			// todo: 得测试下看看 s 会不会空
-			template<size_t len>
-			Escaped& operator=(char const(&s)[len]) {
-				buf = s;
-				this->len = len - 1;
-				return *this;
-			}
 		};
 	}
 
@@ -41,8 +26,8 @@ namespace xx
 	struct SFuncs<T, std::enable_if_t<std::is_base_of_v<xx::MySql::Escaped, T>>> {
 		static inline void WriteTo(std::string& s, T const& in) noexcept {
 			auto&& len = s.size();
-			s.resize(len + in.len * 2);
-			auto&& n = mysql_real_escape_string(in.ctx, (char*)s.data() + len, in.buf, (unsigned long)in.len);
+			s.resize(len + in.s.size() * 2);
+			auto&& n = mysql_escape_string((char*)s.data() + len, (char*)in.s.data(), (unsigned long)in.s.size());
 			s.resize(len + n);
 		}
 	};
@@ -178,14 +163,14 @@ namespace xx
 
 			// 一次填充多个
 			template<typename...Args>
-			inline void Reads(Args& ...args) {
+			void Reads(Args& ...args) {
 				int colIdx = 0;
 				ReadsCore(colIdx, args...);
 			}
 
 		protected:
 			template<typename Arg, typename...Args>
-			inline void ReadsCore(int& colIdx, Arg& arg, Args& ...args) {
+			void ReadsCore(int& colIdx, Arg& arg, Args& ...args) {
 				Read(colIdx, arg);
 				ReadsCore(++colIdx, args...);
 			}
@@ -199,86 +184,75 @@ namespace xx
 		struct Connection {
 			MYSQL* ctx = nullptr;
 			std::string lastError;
-			unsigned int lastErrorNumber = 0;
+			unsigned int lastErrorNumber = 0;			// try 到的异常代码仅用于表达出错的位置. 这个是具体的错误码
 
-			operator bool() {
+			inline operator bool() {
 				return ctx;
 			}
 
 			Connection() = default;
+			Connection(Connection const&) = delete;
+			Connection(Connection&& o) {
+				operator=(std::move(o));
+			}
+			Connection& operator=(Connection const&) = delete;
+			Connection& operator=(Connection && o) {
+				std::swap(ctx, o.ctx);
+				std::swap(lastError, o.lastError);
+				std::swap(lastErrorNumber, o.lastErrorNumber);
+				return *this;
+			}
 			~Connection() {
 				Close();
 			}
 
-			void Open(char const* const& host, int const& port, char const* const& username, char const* const& password, char const* const& db) {
-				if (ctx) {
-					lastError = "connection is opened.";
-					lastErrorNumber = 0;
-					throw lastErrorNumber;
-				}
+			inline int TryOpen(char const* const& host, int const& port, char const* const& username, char const* const& password, char const* const& db) noexcept {
+				Close();
 				ctx = mysql_init(nullptr);
 				if (!ctx) {
 					lastError = "mysql_init failed.";
 					lastErrorNumber = -1;
-					throw lastErrorNumber;
+					return -1;
 				}
-				if (!mysql_real_connect(ctx, host, username, password, db, port, nullptr, CLIENT_MULTI_STATEMENTS)) {	// todo: 关 SSL 的参数
+				if (!mysql_real_connect(ctx, host, username, password, db, port, nullptr, CLIENT_MULTI_STATEMENTS)) {	// todo: 关 SSL 的参数, 限制连接超时时长
 					lastError = mysql_error(ctx);
 					lastErrorNumber = mysql_errno(ctx);
-					throw lastErrorNumber;
+					Close();
+					return -2;
 				}
+				my_bool reconnect = 1;
+				return mysql_options(ctx, MYSQL_OPT_RECONNECT, &reconnect);
 			}
 
-			void Close() {
+			inline void Open(char const* const& host, int const& port, char const* const& username, char const* const& password, char const* const& db) {
+				if (int r = TryOpen(host, port, username, password, db)) throw r;
+			}
+
+			inline void Close() {
 				if (ctx) {
 					mysql_close(ctx);
 					ctx = nullptr;
 				}
 			}
 
-			Escaped MakeEscaped() {
-				return Escaped(ctx);
-			}
-
-			int Ping() {
+			inline int Ping() {
 				if (!ctx) return 0;
 				return mysql_ping(ctx);
 			}
 
-			// 执行一段 SQL 脚本. 后续使用 Fetch 检索返回结果( 如果有的话 )
-			void Execute(char const* const& sql, unsigned long const& len) {
+
+
+			// 执行一段 SQL 脚本. 后续使用 Fetch 检索返回结果( 如果有的话 ). 返回 0 表示成功. 非 0 为错误码.
+			inline int TryExecute(char const* const& sql, unsigned long const& len) noexcept {
 				if (!ctx) {
 					lastError = "connection is closed.";
 					lastErrorNumber = 0;
-					throw lastErrorNumber;
+					return -1;
 				}
 				if (mysql_real_query(ctx, sql, len)) {
 					lastError = mysql_error(ctx);
 					lastErrorNumber = mysql_errno(ctx);
-					throw lastErrorNumber;
-				}
-			}
-
-			template<size_t len>
-			void Execute(char const(&sql)[len]) {
-				Execute(sql, (unsigned long)(len - 1));
-			}
-
-			void Execute(std::string const& sql) {
-				Execute((char*)sql.data(), (unsigned long)sql.size());
-			}
-
-			// 执行一段 SQL 脚本. 后续使用 Fetch 检索返回结果( 如果有的话 ). 这是不 throw 版. 返回 0 表示成功. 非 0 为错误码.
-			int TryExecute(char const* const& sql, unsigned long const& len) {
-				if (!ctx) {
-					lastError = "connection is closed.";
-					lastErrorNumber = 0;
-					return lastErrorNumber;
-				}
-				if (mysql_real_query(ctx, sql, len)) {
-					lastError = mysql_error(ctx);
-					lastErrorNumber = mysql_errno(ctx);
-					return lastErrorNumber;
+					return -2;
 				}
 				return 0;
 			}
@@ -288,19 +262,33 @@ namespace xx
 				return TryExecute(sql, (unsigned long)(len - 1));
 			}
 
-			int TryExecute(std::string const& sql) {
+			inline int TryExecute(std::string const& sql) {
 				return TryExecute((char*)sql.data(), (unsigned long)sql.size());
 			}
 
 
+			// 执行一段 SQL 脚本. 后续使用 Fetch 检索返回结果( 如果有的话 ). 出错抛异常
+			inline void Execute(char const* const& sql, unsigned long const& len) {
+				if (int r = TryExecute(sql, len)) throw r;
+			}
 
-			// 填充一个结果集, 并产生相应回调. 返回 是否存在下一个结果集
+			template<size_t len>
+			void Execute(char const(&sql)[len]) {
+				Execute(sql, (unsigned long)(len - 1));
+			}
+
+			inline void Execute(std::string const& sql) {
+				Execute((char*)sql.data(), (unsigned long)sql.size());
+			}
+
+
+			// 填充一个结果集, 并产生相应回调. 出错返回负数. 0 表示没有. 1 表示存在下一个结果集
 			// infoHandler 返回 true 将继续对每行数据发起 rowHandler 调用. 返回 false, 将终止调用
-			bool Fetch(std::function<bool(Info&)>&& infoHandler, std::function<bool(Reader&)>&& rowHandler) {
+			inline int TryFetch(std::function<bool(Info&)>&& infoHandler, std::function<bool(Reader&)>&& rowHandler) {
 				if (!ctx) {
 					lastError = "connection is closed.";
 					lastErrorNumber = 0;
-					throw lastErrorNumber;
+					return -1;
 				}
 
 				Info info(lastError);
@@ -328,7 +316,7 @@ namespace xx
 							if (!reader.lengths) {
 								lastError = mysql_error(ctx);
 								lastErrorNumber = mysql_errno(ctx);
-								throw lastErrorNumber;
+								return -2;
 							}
 							if (!rowHandler(reader)) break;
 						}
@@ -346,20 +334,28 @@ namespace xx
 					// 有结果集 但是出错
 					lastError = mysql_error(ctx);
 					lastErrorNumber = mysql_errno(ctx);
-					throw lastErrorNumber;
+					return -3;
 				}
 
-				// 0: 有更多结果集.  -1: 没有    >0: 出错
+				// n = 0: 有更多结果集.  -1: 没有    >0: 出错
 				auto&& n = mysql_next_result(ctx);
-				if (!n) return true;
-				else if (n == -1) return false;
+				if (!n) return 1;
+				else if (n == -1) return 0;
 				else {
 					lastError = mysql_error(ctx);
 					lastErrorNumber = mysql_errno(ctx);
-					throw lastErrorNumber;
+					return -4;
 				}
-				return false;
+				return 0;
 			};
+
+			// 填充一个结果集, 并产生相应回调. 返回 是否存在下一个结果集. 出错抛异常
+			// infoHandler 返回 true 将继续对每行数据发起 rowHandler 调用. 返回 false, 将终止调用
+			inline bool Fetch(std::function<bool(Info&)>&& infoHandler, std::function<bool(Reader&)>&& rowHandler) {
+				int r = TryFetch(std::move(infoHandler), std::move(rowHandler));
+				if (r < 0) throw r;
+				return (bool)r;
+			}
 		};
 	}
 }
---
+++ xxlib/xx_threadpool_ex.h
@@ -0,0 +1,93 @@
+﻿#pragma once
+#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include "xx_object.h"
+
+namespace xx {
+	template<typename T>
+	struct DefaultErrorHandler {
+		void operator ()(const T& t ,const std::exception& e)
+		{
+			//todo
+		}
+		void operator ()(const T& t, const int& code)
+		{
+			//todo
+		}
+	};
+
+	template<typename T,typename ErrorHandler = DefaultErrorHandler<T>>
+	class ThreadPoolEx {
+		std::vector<std::thread> threads;
+		std::queue<std::function<void(T&)>> jobs;
+		std::mutex mtx;
+		std::condition_variable cond;
+		bool stop = false;
+
+		ErrorHandler eh;
+	public:
+		ThreadPoolEx(int const& numThreads = 4, ErrorHandler&& eh = ErrorHandler())
+		:eh (std::move(eh))
+		{
+			for (int i = 0; i < numThreads; ++i) {
+				threads.emplace_back([this] {
+					T t ;
+					while (true) {
+						std::function<void(T&)> job;
+						{
+							std::unique_lock<std::mutex> lock(this->mtx);
+							this->cond.wait(lock, [this] {
+								return this->stop || !this->jobs.empty();
+								});
+							if (this->stop && this->jobs.empty()) return;
+							job = std::move(this->jobs.front());
+							this->jobs.pop();
+						}
+						try
+						{
+							job(t);
+						}
+						catch (const std::exception& e)
+						{
+							this->eh(t,e);
+						}
+						catch (const int& code)
+						{
+							this->eh(t, code);
+						}
+						catch (...)
+						{
+							//this->eh(t);
+						}
+					}
+					});
+			}
+		}
+
+		int Add(std::function<void(T&)>&& job) {
+			{
+				std::unique_lock<std::mutex> lock(mtx);
+				if (stop) return -1;
+				jobs.emplace(std::move(job));
+			}
+			cond.notify_one();
+			return 0;
+		}
+
+		~ThreadPoolEx() {
+			{
+				std::unique_lock<std::mutex> lock(mtx);
+				stop = true;
+			}
+			cond.notify_all();
+			for (std::thread& worker : threads) {
+				worker.join();
+			}
+		}
+	};
+}
