a3e206fb437f9f4c63a095ee64cd5682d93b375b
denghe <denghe@hotmail.com>
2019-09-05 14:50:30 +0800

    writing

---
+++ test_linux_1/xx_coros_boost.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include "xx_bbuffer.h"
+#include <boost/context/all.hpp>
+
+namespace xx {
+
+	struct Coros;
+	struct Coro {
+		boost::context::continuation c1;
+		boost::context::continuation c2;
+		size_t idx = -1;
+		Coros& coros;
+
+		void operator()() {
+			c2 = c2.resume();
+		}
+
+		Coro(Coros& coros, boost::context::continuation&& c);
+		~Coro();
+		Coro(Coro&&) = delete;
+		Coro& operator=(Coro&&) = delete;
+		Coro(Coro&) = delete;
+		Coro& operator=(Coro&) = delete;
+	};
+
+	struct Coros {
+		List<Coro*> cs;
+
+		void Add(std::function<void(Coro& yield)>&& job, bool runImmediately = false) {
+
+			auto&& c1 = boost::context::callcc([this, job = std::move(job)](boost::context::continuation&& c2) {
+				Coro co(*const_cast<Coros*>(this), std::move(c2));
+				co.c2 = co.c2.resume();
+				job(co);
+				return std::move(co.c2);
+			});
+
+			auto&& c = cs[cs.len - 1]->c1;
+			c = std::move(c1);
+
+			if (runImmediately) {
+				c = c.resume();
+			}
+		}
+
+		void RunOnce() {
+			for (int i = (int)cs.len - 1; i >= 0; --i) {
+				auto&& co = cs[i];
+				co->c1 = co->c1.resume();
+			}
+		}
+
+		void Run() {
+			while (cs.len) {
+				RunOnce();
+			}
+		}
+	};
+
+	inline Coro::Coro(Coros& coros, boost::context::continuation&& c)
+		: c2(std::move(c))
+		, coros(coros)
+	{
+		idx = coros.cs.len;
+		coros.cs.Add(this);
+	}
+
+	inline Coro::~Coro() {
+		coros.cs[coros.cs.len - 1]->idx = idx;
+		coros.cs.SwapRemoveAt(idx);
+	}
+
+}
---
+++ test_linux_1/xx_epoll_buf.h
@@ -0,0 +1,104 @@
+﻿#pragma once
+#include "xx_bbuffer.h"
+#include "xx_queue.h"
+
+namespace xx {
+	// 引用计数放在 buf[len] 的后面. 从 BBuffer 剥离时 对齐追加一个定长 int 的空间但是不改变长度
+	struct EpollBuf {
+
+		// 指向内存块
+		uint8_t* buf;
+
+		// 内存块内有效数据长度
+		size_t len;
+
+		// 指向内存块中引用计数变量
+		int* refs;
+
+		EpollBuf(BBuffer& bb) {
+			Init(bb);
+		}
+
+		inline void Reset(BBuffer& bb) {
+			Dispose();
+			Init(bb);
+		}
+
+		EpollBuf()
+			: buf(nullptr)
+			, len(0)
+			, refs(nullptr)
+		{
+		}
+
+		EpollBuf(EpollBuf const& o)
+			: buf(o.buf)
+			, len(o.len)
+			, refs(o.refs)
+		{
+			++(*refs);
+		}
+
+		inline EpollBuf& operator=(EpollBuf const& o) {
+			if (this == &o) return *this;
+			Dispose();
+			this->buf = o.buf;
+			this->len = o.len;
+			this->refs = o.refs;
+			++(*refs);
+			return *this;
+		}
+
+		~EpollBuf() {
+			Dispose();
+		}
+
+		EpollBuf(EpollBuf&& o)
+			: buf(o.buf)
+			, len(o.len)
+			, refs(o.refs)
+		{
+			o.buf = nullptr;
+			o.len = 0;
+			o.refs = nullptr;
+		}
+
+		EpollBuf& operator=(EpollBuf&& o) {
+			std::swap(this->buf, o.buf);
+			std::swap(this->len, o.len);
+			std::swap(this->refs, o.refs);
+			return *this;
+		}
+
+	protected:
+		inline void Dispose() {
+			if (buf) {
+				if (-- * refs == 0) {
+					::free(buf);
+				}
+				buf = nullptr;
+			}
+		}
+
+		inline void Init(BBuffer& bb) {
+			// 为 refs 扩容
+			bb.Reserve(bb.len + 8);
+
+			// 直读内存信息
+			buf = bb.buf;
+			len = bb.len;
+
+			// 计算出 refs 的 4 字节对齐位置
+			refs = (int*)(((((size_t)buf + len - 1) / 4) + 1) * 4);
+
+			// 内存脱钩
+			bb.Reset();
+		}
+	};
+
+	// 标识内存可移动
+	template<>
+	struct IsTrivial<EpollBuf, void> {
+		static const bool value = true;
+	};
+}
