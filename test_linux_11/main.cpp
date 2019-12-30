9f6913c5ad5026128d69ef65f39d2c0620acf823
denghe <denghe@hotmail.com>
2019-12-30 18:56:51 +0800

    writing...

--- test_linux_11/main.cpp
+++ test_linux_11/main.cpp
@@ -11,23 +11,26 @@
 #include <chrono>
 
 #define ENABLE_HEURISTIC
+// 对于 map1 似乎结果输出不正确. 有空再修修看?
+//#define ENABLE_CELLHEAP2
 
 struct Cell {
 	int x = 0, y = 0;
 	int walkable = 0;
+	int unused = 0;
 
 	int isOpened = 0;
 	int isClosed = 0;
+	float heuristicStartToEndLen = 0.0f;
+	float startToCurNodeLen = 0.0f;
 #ifdef ENABLE_HEURISTIC
 	int heuristicCurNodeToEndLen_hasValue = 0;
 	float heuristicCurNodeToEndLen = 0.0f;
 #endif
-	float heuristicStartToEndLen = 0.0f;
-	float startToCurNodeLen = 0.0f;
 	Cell* parent = nullptr;
 
 	inline void Clear() {
-		memset(&isOpened, 0, sizeof(Cell) - 4 * 3);
+		memset(&isOpened, 0, sizeof(Cell) - 4 * 4);
 	}
 
 	inline void Fill(std::vector<Cell*>& path) {
@@ -40,186 +43,24 @@ struct Cell {
 	}
 };
 
-struct CellComparer {
-	static bool LargeThan(Cell* const& a, Cell* const& b) {
-		return a->heuristicStartToEndLen > b->heuristicStartToEndLen;
-	}
-	static bool LessThan(Cell* const& a, Cell* const& b) {
-		return a->heuristicStartToEndLen < b->heuristicStartToEndLen;
-	}
-
-	bool operator() (const Cell* x, const Cell* y) const noexcept {
-		return x->heuristicStartToEndLen > y->heuristicStartToEndLen;
-	}
-};
-
-
-template<typename T = Cell*, typename C = CellComparer>
-struct IntervalHeap {
-	struct Interval {
-		T first = nullptr;
-		T last = nullptr;
-	};
-	std::vector<Interval> heap;
-	int size = 0;
-
-	void Clear() {
-		size = 0;
-		memset(heap.data(), 0, sizeof(Interval) * heap.size());
-	}
-
-	void swapFirstWithLast(int const& cell1, int const& cell2) {
-		auto first = heap[cell1].first;
-		updateFirst(cell1, heap[cell2].last);
-		updateLast(cell2, first);
-	}
-
-	void swapFirstWithFirst(int const& cell1, int const& cell2) {
-		auto first = heap[cell2].first;
-		updateFirst(cell2, heap[cell1].first);
-		updateFirst(cell1, first);
-	}
-
-	bool heapifyMin(int const& cell) {
-		bool result = false;
-		if (2 * cell + 1 < size && C::LargeThan(heap[cell].first, heap[cell].last)) {
-			result = true;
-			swapFirstWithLast(cell, cell);
-		}
-		int num = cell;
-		int num2 = 2 * cell + 1;
-		int num3 = num2 + 1;
-		if (2 * num2 < size && C::LessThan(heap[num2].first, heap[num].first)) {
-			num = num2;
-		}
-		if (2 * num3 < size && C::LessThan(heap[num3].first, heap[num].first)) {
-			num = num3;
-		}
-		if (num != cell) {
-			swapFirstWithFirst(num, cell);
-			heapifyMin(num);
-		}
-		return result;
-	}
-
-	void bubbleUpMin(int i) {
-		if (i > 0) {
-			auto first = heap[i].first;
-			auto val = first;
-			int num = (i + 1) / 2 - 1;
-			while (i > 0 && C::LessThan(val, (first = heap[num = (i + 1) / 2 - 1].first))) {
-				updateFirst(i, first);
-				first = val;
-				i = num;
-			}
-			updateFirst(i, val);
-		}
-	}
-
-	void bubbleUpMax(int i) {
-		if (i > 0) {
-			auto last = heap[i].last;
-			auto val = last;
-			int num = (i + 1) / 2 - 1;
-			while (i > 0 && C::LargeThan(val, (last = heap[num = (i + 1) / 2 - 1].last))) {
-				updateLast(i, last);
-				last = val;
-				i = num;
-			}
-			updateLast(i, val);
-		}
-	}
-
-	IntervalHeap(int const& capacity = 1024) {
-		heap.resize(capacity);
-	}
-
-	void Add(T const& item) {
-		if (size == 0) {
-			size = 1;
-			updateFirst(0, item);
-			return;
-		}
-		if (size == 2 * (int)heap.size()) {
-			heap.resize(size);
-		}
-		if (size % 2 == 0) {
-			int num = size / 2;
-			int num2 = (num + 1) / 2 - 1;
-			auto last = heap[num2].last;
-			if (C::LargeThan(item, last)) {
-				updateFirst(num, last);
-				updateLast(num2, item);
-				bubbleUpMax(num2);
-			}
-			else {
-				updateFirst(num, item);
-				if (C::LessThan(item, heap[num2].first)) {
-					bubbleUpMin(num);
-				}
-			}
-		}
-		else {
-			int num3 = size / 2;
-			auto first = heap[num3].first;
-			if (C::LessThan(item, first)) {
-				updateLast(num3, first);
-				updateFirst(num3, item);
-				bubbleUpMin(num3);
-			}
-			else {
-				updateLast(num3, item);
-				bubbleUpMax(num3);
-			}
-		}
-		size++;
-	}
-
-	void updateLast(int const& cell, T const& item) {
-		heap[cell].last = item;
-	}
-
-	void updateFirst(int const& cell, T const& item) {
-		heap[cell].first = item;
-	}
-
-	T DeleteMin() {
-		if (size == 0) {
-			throw - 1;
-		}
-		auto first = heap[0].first;
-		if (size == 1) {
-			size = 0;
-			heap[0].first = nullptr;
-		}
-		else {
-			int num = (size - 1) / 2;
-			if (size % 2 == 0) {
-				updateFirst(0, heap[num].last);
-				heap[num].last = nullptr;
-			}
-			else {
-				updateFirst(0, heap[num].first);
-				heap[num].first = nullptr;
-			}
-			size--;
-			heapifyMin(0);
-		}
-		return first;
-	}
-};
-
 struct CellHeap {
-	CellComparer comparer;
+	struct Comparer {
+		bool operator() (Cell const* x, Cell const* y) const noexcept {
+			return x->heuristicStartToEndLen > y->heuristicStartToEndLen;
+		}
+	} comparer;
+
 	std::vector<Cell*> data;
+
 	void Clear() {
 		data.clear();
 	}
-	size_t size() const {
-		return data.size();
+
+	bool Empty() const {
+		return data.empty();
 	}
+
 	void Add(Cell* const& c) {
-		//c->isOpened = 1;
 		data.emplace_back(c);
 		std::push_heap(data.begin(), data.end(), comparer);
 	}
@@ -228,8 +69,50 @@ struct CellHeap {
 		auto from = data.front();
 		std::pop_heap(data.begin(), data.end(), comparer);
 		data.pop_back();
-		from->isOpened = 0;	// 该不该清??
 		return from;
+	}
+};
+
+struct CellHeap2 {
+	std::vector<Cell*> data;
+
+	void Clear() {
+		data.clear();
+	}
+
+	bool Empty() const {
+		return data.empty();
+	}
+
+	void Add(Cell* const& c) {
+		int p = (int)data.size(), p2;
+		data.push_back(c);
+		do {
+			if (p == 0) break;
+			p2 = (p - 1) / 2;
+			if (data[p]->heuristicStartToEndLen < data[p2]->heuristicStartToEndLen) {
+				std::swap(data[p], data[p2]);
+				p = p2;
+			}
+			else break;
+		} while (true);
+	}
+
+	Cell* DeleteMin() {
+		auto result = data[0];
+		int p = 0, p1, pn;
+		data[0] = data[data.size() - 1];
+		data.pop_back();
+		do {
+			pn = p;
+			p1 = 2 * p + 1;
+			if ((int)data.size() > p1 && data[p]->heuristicStartToEndLen > data[p1]->heuristicStartToEndLen) {
+				p = p1;
+			}
+			if (p == pn) break;
+			std::swap(data[p], data[pn]);
+		} while (true);
+		return result;
 	}
 };
 
@@ -251,8 +134,11 @@ struct Grid {
 		, std::pair<int, int>{1, 1}
 	};
 	const float sqrt_2 = sqrtf(2);
-	//IntervalHeap<> openList;
+#ifdef ENABLE_CELLHEAP2
+	CellHeap2 openList;
+#else
 	CellHeap openList;
+#endif
 
 	Cell& At(int const& x, int const& y) {
 		assert(x >= 0 && y >= 0 && x < width && y < height);
@@ -261,8 +147,8 @@ struct Grid {
 
 #ifdef ENABLE_HEURISTIC
 	static float Heuristic(Cell* const& a, Cell* const& b) {
-		return (std::abs(a->x - b->x) + std::abs(a->y - b->y));
-		//return sqrtf(float((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y)));
+		//return (std::abs(a->x - b->x) + std::abs(a->y - b->y));
+		return sqrtf(float((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y)));
 	}
 #endif
 
@@ -273,9 +159,9 @@ struct Grid {
 		openList.Add(startCell);
 		startCell->isOpened = 1;
 
-		while (openList.size()) {
-			//auto cell = openList.DeleteMin();
+		while (!openList.Empty()) {
 			auto cell = openList.DeleteMin();
+			cell->isOpened = 0;
 			cell->isClosed = 1;
 
 			if (cell == endCell) {
@@ -398,7 +284,7 @@ int64_t NowMS() {
 
 int main() {
 	Grid g;
-	g.LoadByFile("map3.txt");
+	g.LoadByFile("map1.txt");
 
 	auto ms = NowMS();
 	int count = 0;
